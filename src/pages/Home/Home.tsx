d8085202afc92d50f7e0116c3d557133ff656806
Julian Rodriguez <jsantosr134@gmail.com>
2022-10-26 23:36:37 -0500

    Manejo de La seleccion de personas favoritas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,34 @@
-import {DataGrid, GridRenderCellParams} from '@mui/x-data-grid';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
 import { People } from "@/data/people";
-import {useState} from "react";
-import {Person} from "@/models/people";
-import {Checkbox} from "@mui/material";
+import { useState } from "react";
+import { Person } from "@/models/people";
+import { Checkbox } from "@mui/material";
 
 export const Home = () => {
     const [ selectedPeople, setSelectedPeople ] = useState<Person[]>([]);
     const pageSizes = 5;
+
+    const findPerson = ( person: Person ) => !!selectedPeople.find( p => p.id === person.id );
+    const filterPerson = ( person: Person ) => selectedPeople.filter( p => p.id !== person.id );
+
+    const handleChange = ( person: Person ) => {
+        setSelectedPeople( findPerson( person ) ? filterPerson( person ) : [ ...selectedPeople, person ] );
+    }
+
     const columns = [
         {
             field: 'actions',
+            type: 'actions',
+            sortable: false,
             headerName: '',
             width: 50,
             renderCell: ( params: GridRenderCellParams ) => (
                 <>
                     {/* !! = con esto transformamos la resolución de este metodo en un true o false */}
-                    <Checkbox size="small" checked={ !!selectedPeople.find( p => p.id === params.row.id ) } />
+                    <Checkbox size="small"
+                              checked={ findPerson( params.row ) }
+                              onChange={ () => handleChange( params.row ) }
+                    />
                 </>
             )
         },
